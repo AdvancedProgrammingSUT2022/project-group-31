bcdf530a416c67b3b85372c17dc0fac4dfdd06ee
Amin-tay <mohammadamintayebi1@gmail.com>
2022-04-24 00:17:02 +0430

    add: make database singleton

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -5,6 +5,14 @@ import Enums.HEX;
 import java.util.ArrayList;
 
 public class Database {
+    private  static  Database instance = null;
+    private  Database(){}
+    public static Database getInstance(){
+        if(instance == null)
+            instance = new Database();
+        return instance;
+
+    }
     ArrayList<User> users;
     ArrayList<HEX> map;
 
