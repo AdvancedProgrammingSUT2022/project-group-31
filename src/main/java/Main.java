9869918eab01ecbd1b0d2e91e9fb6e35b7a5bd30
Amin-tay <mohammadamintayebi1@gmail.com>
2022-05-03 13:14:00 +0430

    new map added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,23 +1,12 @@
-import Enums.Message;
 import dk.ilios.asciihexgrid.AsciiBoard;
 import dk.ilios.asciihexgrid.printers.LargePointyAsciiHexPrinter;
-
-import model.AsciiBoard;
-import view.LoginMenu;
+import view.PrintMap;
 
 public class Main {
     public static void main(String[] args) {
-        AsciiBoard board = new AsciiBoard(0, 3, 0, 2 , new LargePointyAsciiHexPrinter());
-        board.printHex("1234567899","-B-", '#', 0, 0);
-        board.printHex("HX2","-W-", '+', 1, 0);
-        board.printHex("HX3","-W-", '-', 2, 0);
-        board.printHex("HX4","-B-", '.', 3, 0);
-        board.printHex("HX5","-B-", '#', 0, 1);
-        board.printHex("HX6","-W-", '+', 1, 1);
-        board.printHex("HX7","-W-", '-', 2, 1);
-        board.printHex("HX8","-B-", '.', 3, 1);
-        //  board.printHex();
-        System.out.println(board.prettPrint(true));
+
+        PrintMap printMap = new PrintMap();
+        printMap.run();
         //LoginMenu.run();
     }
 }
